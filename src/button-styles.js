0c0b97126e44d28c5513d8bd490925ba03d07b7c
Ashish Dutta <ashish.dutta@medlife.com>
2020-09-24 03:52:56 +0530

    webpack setup basic stuff - 03-fem-debug-script

---
+++ src/button-styles.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {string} color
+ * @returns {string}
+ */
+const makeColorStyle = (color) => `color: ${color}`;
+const red = "red";
+const blue = "blue";
+
+exports.red = red;
+exports.blue = blue;
+exports.makeColorStyle = makeColorStyle;
